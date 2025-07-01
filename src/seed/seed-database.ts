f2671e688937677a934d12b8f10d91550371853f
franchierchie <franchierchie@gmail.com>
2025-07-01 17:34:18 -0300

    DB setup

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,60 @@
+import { prisma } from '../lib/prisma';
+import { initialData } from './seed';
+
+async function main() {
+  // Delete previous registries
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+
+  // Categories
+  const categoriesData = categories.map(category => ({
+    name: category
+  }));
+
+  await prisma.category.createMany({
+    data: categoriesData
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLocaleLowerCase()] = category.id;
+
+    return map;
+  }, {} as Record<string, string>); // <string=shirt, string=categoryID>
+
+
+  // Products
+  products.forEach(async(product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type]
+      }
+    });
+    
+      // Images
+      const imagesData = images.map(image => ({
+        url: image,
+        productId: dbProduct.id,
+      }))
+
+      await prisma.productImage.createMany({
+        data: imagesData
+      });
+  });
+
+
+  console.log('Seed executed successfully');
+}
+
+(() => {
+  if ( process.env.NODE_ENV === 'production' ) return;
+
+  main();
+})();
